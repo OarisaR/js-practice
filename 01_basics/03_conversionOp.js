31843b94f3d27f1aca3b3c5c03fcd3da3afa8e50
Oarisa Rebayet <151024492+OarisaR@users.noreply.github.com>
2025-03-06 11:00:03 +0000

    operations

--- 01_basics/03_conversionOp.js
+++ 01_basics/03_conversionOp.js
@@ -32,3 +32,35 @@ let someNum = 33
 let stringNum = String(someNum)
 console.log(typeof stringNum) //string
 
+/* Operations */
+
+let value = 3
+let neg = -value;
+//console.log(neg) // -3
+
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1+str2;
+
+//Compleixty arises when we add num and string
+console.log(1+"2") // 12
+console.log("1"+2) //12
+console.log("1"+2+2) //122
+console.log(1+2+"2") //32
+//Basically the type which appears first gets priority !
+
+
+console.log(+true) // 1 -> conversion to num!
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+let b = ++gameCounter
+let c = gameCounter++
+console.log(b);
+console.log(c);
+console.log(gameCounter);
+
+
+
