c897323428e4ab87ad9fc9c6251f84b53bc49270
Oarisa Rebayet <151024492+OarisaR@users.noreply.github.com>
2025-03-06 10:19:06 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 2104129
+let accountEmail = "jsprac@vivo.com"
+var accountPassword = "12345"
+accountCity = "Dhaka"
+
+let accountState;
+//accountId = 2  --> not allowed
+// let is a better variable type as it has a local scope 
+// var has a functional scope so it is better not to use it!
+
+
+
+accountEmail - "hchc@g.com"
+accountPassword = "1234"
+accountCity = "Sylhet"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
