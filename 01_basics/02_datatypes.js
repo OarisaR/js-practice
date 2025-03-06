9c556dc7c1b0ed301f7c71d2cba9cef124e5cff2
Oarisa Rebayet <151024492+OarisaR@users.noreply.github.com>
2025-03-06 10:35:50 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all  JS code as newer version
+//however modern JS engines have this by default but writing this is a good practice
+
+//alert(3+3) --> this won't be executed as we are using node js not a browser
+
+let name  = "Oari"
+let age = 21
+let isLoggedIn = false
+
+// number -> 2 ** 53 
+// bigint
+// string -> ' ' / " "   double quotes is preferred
+// boolean -> true/false
+// null -> standalonen value
+// undefined -> value is not assigned
+// symbol -> unique
+
+//object
+console.log(typeof age);
