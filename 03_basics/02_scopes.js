b8ec44f8e7ee104a51223269b97e67ef7537ba0a
Oarisa Rebayet <151024492+OarisaR@users.noreply.github.com>
2025-03-09 11:06:36 +0000

    scope2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -14,3 +14,42 @@ console.log(c);
 
 //BEST TO AVOID VAR!
 
+
+
+// -----NESTED SCOPE -----
+
+function one(){
+    const user = "oarisa"
+
+    function two(){
+        const website = "Imagelicious"
+        console.log(user);
+        
+    }
+    two() // runs as two is defined within on so user is acessible by it
+    //console.log(website); err
+    
+    
+}
+
+one() ; // SHOWS ERROR! AS CONST HAS BLOCK SCOPE it will say website is not defined
+// CHILD FUNC CAN ACCESS PARENT VARIABLES
+
+
+
+
+
+addone(2); //no error even though it is being called before declaration
+function addone(num){
+    return num + 1;
+}
+
+
+
+
+//addTwo(2)  //error
+const addTwo = function(num){ 
+    return num + 2;
+}
+
+addTwo(2)
